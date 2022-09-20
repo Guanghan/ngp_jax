d5429370dcda27db11068be52ba94039e85fa78e
Guanghan Ning <guanghan.ning@gmail.com>
2022-09-20 11:01:35 -0700

    trilinear interp

---
+++ CUDA_JAX/include/tri_interp.h
@@ -0,0 +1,4 @@
+#include <cuda_runtime_api.h>
+
+#include <cstddef>
+#include <cstdint>
---
+++ CUDA_JAX/lib/tri_interp.cu
@@ -0,0 +1,3 @@
+// This file contains the GPU implementation of trilinear interpolation op.
+
+#include <include/tri_interp.h>
